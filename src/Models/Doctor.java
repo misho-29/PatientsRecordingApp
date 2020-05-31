cf49b5c1a4653f33097110708d84457bcccd9d30
misho jikurashvili <oonneexx@gmail.com>
2020-05-31 17:09:56 +0400

    Added getters and setters

--- src/Models/Doctor.java
+++ src/Models/Doctor.java
@@ -1,4 +1,13 @@
 package Models;
 
 public class Doctor extends Person {
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int Id;
 }
--- src/Models/Person.java
+++ src/Models/Person.java
@@ -1,7 +1,22 @@
 package Models;
 
 public class Person {
-    public int Id;
+    public String getFirsName() {
+        return FirsName;
+    }
+
+    public void setFirsName(String firsName) {
+        FirsName = firsName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
     public String FirsName;
     public String LastName;
 }
