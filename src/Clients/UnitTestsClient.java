a7adfc844fac07dd4fffb0bda4257703816cc888
misho jikurashvili <oonneexx@gmail.com>
2020-06-10 22:59:36 +0400

    moved socket out in streams

--- src/Clients/UnitTestsClient.java
+++ src/Clients/UnitTestsClient.java
@@ -17,17 +17,17 @@ public class UnitTestsClient {
     private static final String hostname = "127.0.0.1";
     private static final int portNumber = 44444;
 
+    Socket clientSocket = new Socket(hostname, portNumber);
+    OutputStream outputStream = clientSocket.getOutputStream();
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+    InputStream inputStream = clientSocket.getInputStream();
+    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+    public UnitTestsClient() throws IOException {
+    }
+
     @Test
     public void addDoctorRequestTest() throws UnknownHostException,IOException,ClassNotFoundException{
-        Socket clientSocket;
-
-        clientSocket = new Socket(hostname, portNumber);
-
-        OutputStream outputStream = clientSocket.getOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        InputStream inputStream = clientSocket.getInputStream();
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-
         AddDoctorRequest addDoctorRequest = new AddDoctorRequest();
         addDoctorRequest.Doctor.setFirstName("lasha");
         addDoctorRequest.Doctor.setLastName("khoriashvili");
@@ -35,8 +35,6 @@ public class UnitTestsClient {
         objectOutputStream.writeObject(addDoctorRequest);// gagzavnaa
         addDoctorRequest = (AddDoctorRequest) objectInputStream.readObject(); //pasuxis migeba
         System.out.println("Server response: " + addDoctorRequest.Response);
-
-        clientSocket.close();
 
         assertEquals(true , addDoctorRequest.Response.IsCompleted);
     }
